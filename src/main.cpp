6a49e00e093814ec2c3b5e96d0e67f4b418c247d
Tiberias23 <tiberias.peters@gmail.com>
2025-10-19 02:54:50 +0200

    Removed a TODO :3

--- src/main.cpp
+++ src/main.cpp
@@ -35,4 +35,3 @@ int main(const int argc, char *argv[]) {
 // TODO: Besseres Logging hinzufügen. (Maybe with a flagg?)
 // TODO: Unit-Tests für einzelne Module schreiben.
 // TODO: Performance-Optimierungen prüfen, insbesondere bei der GIF-Verarbeitung.
-// TODO: Dokumentation vereinheitlichen auf eine sprache.
