c67f6cf7acc125d8ce5e161cd07d7364386a5b2a
Tiberias23 <tiberias.peters@gmail.com>
2026-01-21 14:46:43 +0100

    made some performance improvements through call cout just once per line :3

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 #include <stb_image.h>
 
 // ASCII-Zeichen von dunkel → hell
-const std::string ASCII = "@%#*+=-:. ";
+std::string ASCII;
 
 /**
  * @brief converts the brightness from the img to the corresponding ascii value from the ASCII var
@@ -23,25 +23,27 @@ inline char brightness_to_ascii(const unsigned char r, const unsigned char g, co
 }
 
 /**
- * @brief output helper-funktion to output the ascii
+ * @brief output helper-funktion to convert to ascii
  * @param c the char to use
  * @param r the color value for red
  * @param g the color value for green
  * @param b the color value for blue
  * @param color if the output should be colored
  */
-inline void print_ascii(char c, unsigned char r, unsigned char g, unsigned char b, bool color) {
+inline std::string convert_to_ascii(char c, unsigned char r, unsigned char g, unsigned char b, bool color) {
+    std::string output;
     if (color) {
         // 24-bit Truecolor: \033[38;2;<r>;<g>;<b>m
-        std::cout << "\033[38;2;"
-                  << static_cast<int>(r) << ";"
-                  << static_cast<int>(g) << ";"
-                  << static_cast<int>(b) << "m"
-                  << c
-                  << "\033[0m";
+        output.append("\033[38;2;")
+      .append(std::to_string(r)).append(";")
+      .append(std::to_string(g)).append(";")
+      .append(std::to_string(b)).append("m")
+      .append(1, c)
+      .append("\033[0m");
     } else {
-        std::cout << c;
+        output = std::string(1, c);
     }
+    return output;
 }
 
 
@@ -52,7 +54,8 @@ int main(const int argc, char** argv) {
     ("img", "The image to load", cxxopts::value<std::string>()->default_value("Silly_Cat_Character.jpg"))
     ("w,width", "Target output width", cxxopts::value<int>()->default_value("0"))
     ("h,height", "Target output height", cxxopts::value<int>()->default_value("0"))
-    ("c,color", "Enable ANSI truecolor output");
+    ("c,color", "Enable ANSI truecolor output")
+    ("ascii", "change the ASCII alphabet to use", cxxopts::value<std::string>()->default_value("@%#*+=-:. "));
 
     // setting values from the CLI Part
     const auto choices = options.parse(argc, argv);
@@ -63,6 +66,7 @@ int main(const int argc, char** argv) {
 
     // because the default image is set in the option we cann ignore the case that img is not provided
     const std::string img_path = choices["img"].as<std::string>();
+    ASCII = choices["ascii"].as<std::string>();
     const bool use_color = choices.count("color") > 0;
     int target_width  = choices["width"].as<int>();
     int target_height = choices["height"].as<int>();
@@ -108,6 +112,7 @@ int main(const int argc, char** argv) {
 
     // Rendering
     for (int y = 0; y < target_height; ++y) {
+        std::string line;
         for (int x = 0; x < target_width; ++x) {
 
             int src_x = static_cast<int>(static_cast<float>(x) * scale_x);
@@ -124,12 +129,11 @@ int main(const int argc, char** argv) {
 
             const char ascii = brightness_to_ascii(r, g, b);
 
-            print_ascii(ascii, r, g, b, use_color);
+            line.append(convert_to_ascii(ascii, r, g, b, use_color));
         }
-        std::cout << '\n';
+        std::cout << line << "\033[0m\n";
     }
 
-    std::cout << "\033[0m" << std::endl;
     stbi_image_free(img);
     return 0;
 }
