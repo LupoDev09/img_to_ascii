06953cb9aa12b86efa27bb62dc0d5948bdbdc8ae
Tiberias23 <tiberias.peters@gmail.com>
2026-01-22 20:17:58 +0100

    refactor: removed unnecessary include in main.cpp and removed stb_image_write.h because I don't use it :3

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,6 @@
 #include <algorithm>
 #include <atomic>
 #include <mutex>
-#include <ranges>
 #include <cctype>
 
 // Provided header
@@ -320,7 +319,7 @@ int main(const int argc, char** argv) {
         ("v,verbose", "activate verbose mode")
         ("no-output", "render but do not print anything to the console");
 
-    const auto choices = options.parse(argc, argv);
+    const cxxopts::ParseResult choices = options.parse(argc, argv);
     // handle help flag
     if (choices.count("help")) {
         // Basic groups
@@ -341,7 +340,7 @@ int main(const int argc, char** argv) {
                   << "because then you will see mostly the ANSI escapes.\n"
                   << "Rather use something like 'cat' to see the image.\n"
                   << "And I would not use the GIF option with --write-output-to-file,\n"
-                  << "because then you just see the frames. Also, don't combine with --color.\n";
+                  << "because then you just see the frames. Also, don't combine with --color." << std::endl;
         return 0;
     }
 
